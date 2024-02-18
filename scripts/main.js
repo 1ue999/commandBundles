b8d824164e07410b78fe2b3f48af7d7d8924cc1e
1ue999 <106450442+1ue999@users.noreply.github.com>
2024-02-18 20:44:38 +0100

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,3 @@
-function router(e){Http.get("https://raw.githubusercontent.com/1ue999/cmdBundles/main/"+e+".js",(res)=>{eval(res.getResultAsString());})}
+Events.on(openConsole,(router)=>{
+function router(e){Http.get("https://raw.githubusercontent.com/1ue999/cmdBundles/main/"+e+".js",(res)=>{eval(res.getResultAsString());})};
+});
