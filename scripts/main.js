54e6a62f769d578116cbf27738d7a948a9f5d8c6
1ue999 <106450442+1ue999@users.noreply.github.com>
2024-02-26 20:28:46 +0100

    123 test

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,2 @@
 this.router=function(routerrouterrouter){Http.get("https://raw.githubusercontent.com/1ue999/cmdBundles/main/"+routerrouterrouter+".js",(res)=>{eval(res.getResultAsString());})};
+print('commandBundles is initialised.');
