f097f59c739081822898a63df7678144919e3f65
1ue999 <106450442+1ue999@users.noreply.github.com>
2024-05-07 18:10:21 +0200

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,6 @@
+Events.run(Trigger.newGame,()=>{
+  Http.get("https://raw.githubusercontent.com/1ue999/cmdBundles/main/init.js",(res)=>{eval(res.getResultAsString());});
+});
 this.router=function(routerrouterrouter){Http.get("https://raw.githubusercontent.com/1ue999/cmdBundles/main/"+routerrouterrouter+".js",(res)=>{eval(res.getResultAsString());})};
 this.locks=function(thing){Object.keys(thing).forEach(q=>print(q));};
 print('commandBundles is initialised.');
