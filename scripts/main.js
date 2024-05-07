2a93f2cd644568c23f3ea6b6fd544a5295061e5e
1ue999 <106450442+1ue999@users.noreply.github.com>
2024-05-07 17:42:22 +0200

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,3 @@
 this.router=function(routerrouterrouter){Http.get("https://raw.githubusercontent.com/1ue999/cmdBundles/main/"+routerrouterrouter+".js",(res)=>{eval(res.getResultAsString());})};
-this.locks=function(thing){Object.keys(thing).forEach(couterthing=>print(counterthing))};
+this.locks=function(thing){Object.keys(thing).forEach(q=>print(q));};
 print('commandBundles is initialised.');
